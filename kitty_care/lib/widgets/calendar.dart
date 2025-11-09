c9186d10534d5feece379a6511918e6583e0be11
Amal Bilal <amalbilal20@gmail.com>
2025-11-09 07:26:15 -0500

    improve ui

--- kitty_care/lib/widgets/calendar.dart
+++ kitty_care/lib/widgets/calendar.dart
@@ -8,13 +8,11 @@ class Calendar extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onPressed,
-      child: Transform.translate(
-        offset: const Offset(45, -280), // ← X = left, Y = up
-        child: Image.asset(
-          'assets/images/strawberry_calendar.png',
-          height: 100,
-          width: 100,
-        ),
+      behavior: HitTestBehavior.translucent, // ensures taps register
+      child: Image.asset(
+        'assets/images/strawberry_calendar.png',
+        height: 100,
+        width: 100,
       ),
     );
   }
